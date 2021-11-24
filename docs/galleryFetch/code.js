d0823066d4a5e378f1e6a65db9fcd8715b67f9f1
KrowaWeMgle <92986633+KrowaWeMgle@users.noreply.github.com>
2021-11-24 15:13:35 +0100

    A lot push

--- docs/galleryFetch/code.js
+++ docs/galleryFetch/code.js
@@ -1,18 +1,14 @@
-//Gallery View getters
-const galleryView = document.getElementById('galleryView');
-const imgArea = document.getElementById('imgView');
-const buttonNext = document.getElementById('buttonNext');
-const buttonBack = document.getElementById('buttonBack');
-const textImgArea = document.getElementById('galleryText');
-const buttonClose = document.getElementById('buttonClose');
+//Gallery View getter
+getHtml = {
+    imgView: document.querySelector('.gallerView > img'),
+    prevBtn: document.querySelector('.gallerVIew > button[name="prevButton"]'),
+    nextBtn: document.querySelector('.gallerVIew > button[name="nextButton"]'),
+    
 
-//Gallery Thumbnail getters
-const galleryThumbnail = document.getElementById('galleryThumbnail');
-
+};
 
 //Global Img Table of src
 const imgTable = [];
-let maxTable = 6;
 let currentImgNum;
 
 //onload functions
@@ -21,21 +17,13 @@ window.onload = () => {
 addImgToImgTable();
 
 
-//Gallery View
-setImg();
-buttonListiners();
-galleryViewHide();
-
-//Gallery Thumbnail
-addThumbnailHtmlElement();
-
 };
 
 //Global
 //table of img
 const addImgToImgTable = () => {
     
-    for(let i = 0; i<maxTable; i++) {
+    for(let i = 0; i<imgTable.length; i++) {
         imgTable[i] = fetchPhotos(number);
     }
     console.log(imgTable);
